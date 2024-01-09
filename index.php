5f330abd2449f6030698d8f3346da778559682dd
DaviGel <geloso.davide@gmail.com>
2024-01-09 17:02:10 +0100

    feat: bonus 1 in corso

--- index.php
+++ index.php
@@ -1,39 +1,39 @@
 <?php
     $hotels = [
         [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
+          'name' => 'Hotel Belvedere',
+          'description' => 'Hotel Belvedere Descrizione',
+          'parking' => true,
+          'vote' => 4,
+          'distance_to_center' => 10.4
         ],
         [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
+          'name' => 'Hotel Futuro',
+          'description' => 'Hotel Futuro Descrizione',
+          'parking' => true,
+          'vote' => 2,
+          'distance_to_center' => 2
         ],
         [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
+          'name' => 'Hotel Rivamare',
+          'description' => 'Hotel Rivamare Descrizione',
+          'parking' => false,
+          'vote' => 1,
+          'distance_to_center' => 1
         ],
         [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
+          'name' => 'Hotel Bellavista',
+          'description' => 'Hotel Bellavista Descrizione',
+          'parking' => false,
+          'vote' => 5,
+          'distance_to_center' => 5.5
         ],
         [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
+          'name' => 'Hotel Milano',
+          'description' => 'Hotel Milano Descrizione',
+          'parking' => true,
+          'vote' => 2,
+          'distance_to_center' => 50
         ],
     ];
 ?>
@@ -52,6 +52,15 @@
   <body>
     <div class="container mt-5">
       <h1>Hotel</h1>
+      <form action="" method="GET" class="my-4">
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="true" id="flexCheckDefault" name="parking">
+          <label class="form-check-label" for="flexCheckDefault">
+            Parking
+          </label>
+          <button type="submit" class="btn btn-primary btn-sm">Small button</button>
+        </div>
+      </form>
       <table class="table">
         <thead>
           <tr>
@@ -64,13 +73,20 @@
         </thead>
         <tbody>
           <?php
+            $parking = $_GET['parking'] ?? false;
             foreach($hotels as $hotel) {
               echo '<tr>';
               foreach($hotel as $key => $informations) {
-                if($key === 'parking') {
-                  echo ($informations ? "<td>Yes</td>" : "<td>No</td>");
+                if($parking == true) {
+                  if($key === 'parking' && $informations === true) {
+                    echo "<td>$informations</td>";
+                  } 
                 } else {
-                  echo "<td>$informations</td>";
+                  if($key === 'parking') {
+                    echo ($informations ? "<td>Yes</td>" : "<td>No</td>");
+                  } else {
+                    echo "<td>$informations</td>";
+                  }
                 }
               }
               echo '</tr>';
@@ -81,3 +97,17 @@
     </div>
   </body>
 </html>
+
+<!-- 
+  foreach($hotels as $hotel) {
+    echo '<tr>';
+    foreach($hotel as $key => $informations) {
+      if($key === 'parking') {
+        echo ($informations ? "<td>Yes</td>" : "<td>No</td>");
+      } else {
+        echo "<td>$informations</td>";
+      }
+    }
+    echo '</tr>';
+  }
+-->
